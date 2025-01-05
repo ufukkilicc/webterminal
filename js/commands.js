7cf769b505aaa1191d02d654aac6efdc33ad3638
Ufuk Kılıç <103611580+ufukkilicc@users.noreply.github.com>
2025-01-05 17:14:44 +0300

    Added my project

---
+++ js/commands.js
@@ -0,0 +1,157 @@
+var linkedin = "https://www.linkedin.com/in/ufuk-kılıç-111365177/";
+var github = "https://github.com/ufukkilicc";
+var kerbb = "https://kerbb.com";
+var iyony = "https://iyony.com";
+var megaverse = "https://megaverse.coach";
+var email = "mailto:ufukkilicdev@gmail.com";
+var resume = "./public/Resume_en.pdf";
+
+whois = [
+  "<br>",
+  "Hey, I'm ufuk!👋",
+  "I'm a web developer and a person i guess, who builds web based applications and working as fullstack.",
+  "After getting my degree Bachelor of Science in Information Technologies, I worked professionally",
+  "as a Web Developer building marketplace for services web applications mostly for a company.",
+  "Although I am a web developer and programmer, this was not something I initially aimed for.",
+  "However, around the time I graduated, web development technologies were making huge leaps, making",
+  "programming accessible to everyone, and I suddenly found myself in this field. Later, as I continued",
+  "my work, I realized that I had a keen interest in data, and in the future, I aspire to work with",
+  "large-scale data structures like Artificial Intelligence, Big Data, and Cloud systems. I believe that data",
+  "when made meaningful, tells a story, and as humans, what we essentially do on a cognitive level is just",
+  "that—making sense of things and seeing the bigger picture.",
+  "Why I just wrote this a poem, I have no idea. Thank you.",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'linkedin   <a href="https://www.linkedin.com/in/ufuk-kılıç-111365177/" target="_blank">linkedin/ufukkilic</a>',
+  'github     <a href="https://github.com/ufukkilicc" target="_blank">github/ufukkilic</a>',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  '<span class="command">Kerbb</span> Personal          <a href="https://kerbb.com" target="_blank" >kerbb.com</a>',
+  '<span class="command">Job Postings Platform</span>   Sep 2023 - Sep 2022',
+  "<ul>",
+  "<li>Identified the fragmentation in corporate job postings scattered across centralized platform to aggregate these listings, benefiting HR teams, employees, and employers alike.</li>",
+  "<li>Designed and implemented a robust backend infrastructure using NestJS, MongoDB, and Docker, deployed on DigitalOcean, ensuring scalability and high performance, with a fully integrated CI/CD pipeline through GitHub for efficient updates.</li>",
+  "<li>Developed a custom ScraperAPI in JavaScript as the core technology for the platform, automating the daily scraping of job postings from 50 corporate websites. This system ensured real-time updates to the database by dynamically adding or removing listings while adhering to ethical scraping practices.</li>",
+  "</ul>",
+  "<br>",
+  '<span class="command">Megaverse</span> Graf Studios        <a href="https://megaverse.coach" target="_blank" >megaverse.coach</a>',
+  '<span class="command">Online Reservation Platform</span>   Jan 2023 - Jul 2024',
+  "<ul>",
+  "<li>Developed a multi-node architecture with independent databases connected via TCP protocols, ensuring scalability and modularity.</li>",
+  "<li>Built subscription and calendar functionalities, integrating third-party solutions for payments and mailing services.</li>",
+  "<li>Led the design and implementation of CI/CD pipelines using Docker and GitHub Actions to streamline development and deployment.</li>",
+  "<li>Utilized NestJS, React, and MongoDB for creating a secure and real-time communication system, enhancing user experience.</li>",
+  "<li>Collaborated closely with a small team, contributing to both product design and full-stack development.</li>",
+  "</ul>",
+  "<br>",
+  '<span class="command">Iyony</span> Graf Studios            <a href="https://iyony.com" target="_blank" >iyony.com</a>',
+  '<span class="command">Online Teaching Marketplace</span>   Sep 2023 - Sep 2022',
+  "<ul>",
+  "<li>Identified the fragmentation in corporate job postings scattered across centralized platform to aggregate these listings, benefiting HR teams, employees, and employers alike.</li>",
+  "<li>Designed and implemented a robust backend infrastructure using NestJS, MongoDB, and Docker, deployed on DigitalOcean, ensuring scalability and high performance, with a fully integrated CI/CD pipeline through GitHub for efficient updates.</li>",
+  "<li>Developed a custom ScraperAPI in JavaScript as the core technology for the platform, automating the daily scraping of job postings from 50 corporate websites. This system ensured real-time updates to the database by dynamically adding or removing listings while adhering to ethical scraping practices.</li>",
+  "</ul>",
+  "<br>",
+];
+
+experience = [
+  "<br>",
+  '<span class="command">Graf Studios</span>            İzmit, Kocaeli',
+  '<span class="command">Full-Stack Developer</span>    Nov 2022 - Present',
+  "<ul>",
+  "<li>As my first professional role after university, Graf Studios provided a collaborative environment where both the company and team were equally inexperienced, fostering growth through trial and error.</li>",
+  "<li>Contributed to product-level decisions, actively shaping ideas and aligning technical solutions with business needs.</li>",
+  "<li>Managed permissions systems for PowerBI dashboards, enhancing accessibility and user experience.</li>",
+  "</ul>",
+  "<br>",
+];
+
+skills = [
+  "<br>",
+  '<span class="command">NestJS</span>            A progressive Node.js framework for building scalable server-side apps.',
+  '<span class="command">Confluence</span>        A collaboration tool for creating and managing project documentation.',
+  '<span class="command">Jira</span>              A project management tool for agile teams to plan, track, and release software.',
+  '<span class="command">VSCode</span>            A lightweight and versatile code editor by Microsoft.',
+  '<span class="command">Docker</span>            A platform for containerizing applications and running them efficiently.',
+  '<span class="command">Kubernetes</span>        Orchestrating containerized applications for scaling and management.',
+  '<span class="command">System Design</span>     Expertise in designing scalable and reliable system architectures.',
+  '<span class="command">JavaScript</span>        A versatile programming language for web development.',
+  '<span class="command">TypeScript</span>        A superset of JavaScript with added type safety.',
+  '<span class="command">Postman</span>           A tool for testing and documenting APIs.',
+  '<span class="command">GitHub Copilot</span>    A code generation AI to enhance coding productivity.',
+  '<span class="command">Node.js</span>           A runtime environment for executing JavaScript on the server-side.',
+  '<span class="command">MongoDB</span>           A NoSQL database system for flexible and scalable data storage.',
+  '<span class="command">React</span>             A JavaScript library for building user interfaces.',
+  '<span class="command">CI/CD</span>             Automating build, test, and deployment pipelines.',
+  '<span class="command">Microservices</span>     Building modular, distributed systems for scalability.',
+  '<span class="command">GraphQL</span>           A query language for APIs, enabling clients to request exactly the data they need.',
+  '<span class="command">Webpack</span>           A powerful bundler for optimizing assets and managing dependencies.',
+  '<span class="command">TailwindCSS</span>       A utility-first CSS framework for building modern, responsive designs.',
+  '<span class="command">RESTful APIs</span>      Standard API design for communication between frontend and backend.',
+  '<span class="command">WebSockets</span>        Enables real-time, two-way communication between the client and server.',
+  '<span class="command">Redux</span>             A state management library for managing application state.',
+  '<span class="command">OAuth 2.0</span>         An industry-standard protocol for secure authorization.',
+  '<span class="command">MySQL</span>             A widely-used open-source relational database management system.',
+  '<span class="command">Redis</span>             An in-memory data store for caching, messaging, and real-time analytics.',
+  '<span class="command">ELK Stack</span>         A powerful logging and analytics stack (Elasticsearch, Logstash, Kibana).',
+  '<span class="command">Meilisearch</span>       A fast, lightweight search engine for modern web applications.',
+  '<span class="command">DevOps</span>            A culture and practices combining development and IT operations for faster delivery.',
+  '<span class="command">Prometheus</span>        Tools for monitoring and visualizing metrics.',
+  '<span class="command">Grafana</span>           Tools for monitoring and visualizing metrics.',
+  '<span class="command">OpenAPI/Swagger</span>   Standards for designing and documenting RESTful APIs.',
+  '<span class="command">JWT</span>               Secure tokens for transmitting information between parties.',
+  '<span class="command">Kafka</span>             A distributed platform for real-time data streaming.',
+  '<span class="command">CASL</span>              A library for managing application permissions and access control.',
+  '<span class="command">OPA</span>               A policy engine for managing permissions and policies at scale.',
+  '<span class="command">Kuma</span>              A service mesh for managing and securing microservices communication.',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>         Who is ufuk?',
+  '<span class="command">whoami</span>        Who are you?',
+  '<span class="command">social</span>        Display social networks',
+  '<span class="command">projects</span>      View coding projects',
+  '<span class="command">experience</span>    View professional experiences',
+  '<span class="command">skills</span>        View tech stack',
+  '<span class="command">history</span>       View command history',
+  '<span class="command">help</span>          You obviously already know what this does',
+  '<span class="command">email</span>         Do not email me',
+  '<span class="command">resume</span>        As the name suggests it is my resume',
+  '<span class="command">clear</span>         Clear terminal',
+  '<span class="command">banner</span>        Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Just ufuk not a corporation. All knights reserved.</span>',
+  "//  ▗▖ ▗▖▗▄▄▄▖▗▖ ▗▖▗▖ ▗▖    ▗▖ ▗▖▗▄▄▄▖▗▖   ▗▄▄▄▖ ▗▄▄▖                 ",
+  "//  ▐▌ ▐▌▐▌   ▐▌ ▐▌▐▌▗▞▘    ▐▌▗▞▘  █  ▐▌     █  ▐▌                    ",
+  "//  ▐▌ ▐▌▐▛▀▀▘▐▌ ▐▌▐▛▚▖     ▐▛▚▖   █  ▐▌     █  ▐▌                    ",
+  "//  ▝▚▄▞▘▐▌   ▝▚▄▞▘▐▌ ▐▌    ▐▌ ▐▌▗▄█▄▖▐▙▄▄▖▗▄█▄▖▝▚▄▄▖                 ",
+  "//  ▗▖ ▗▖▗▄▄▄▖▗▄▄▖     ▗▄▄▄  ▗▄▄▄▖▗▖  ▗▖▗▄▄▄▖▗▖    ▗▄▖ ▗▄▄▖ ▗▄▄▄▖▗▄▄▖ ",
+  "//  ▐▌ ▐▌▐▌   ▐▌ ▐▌    ▐▌  █ ▐▌   ▐▌  ▐▌▐▌   ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌",
+  "//  ▐▌ ▐▌▐▛▀▀▘▐▛▀▚▖    ▐▌  █ ▐▛▀▀▘▐▌  ▐▌▐▛▀▀▘▐▌   ▐▌ ▐▌▐▛▀▘ ▐▛▀▀▘▐▛▀▚▖",
+  "//  ▐▙█▟▌▐▙▄▄▖▐▙▄▞▘    ▐▙▄▄▀ ▐▙▄▄▖ ▝▚▞▘ ▐▙▄▄▖▐▙▄▄▖▝▚▄▞▘▐▌   ▐▙▄▄▖▐▌ ▐▌",
+  "//  ▗▄▄▖  ▗▄▖ ▗▄▄▖▗▄▄▄▖▗▄▄▄▖ ▗▄▖ ▗▖   ▗▄▄▄▖ ▗▄▖                       ",
+  "//  ▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌ █  ▐▌   ▐▌ ▐▌▐▌     █  ▐▌ ▐▌                      ",
+  "//  ▐▛▀▘ ▐▌ ▐▌▐▛▀▚▖ █  ▐▛▀▀▘▐▌ ▐▌▐▌     █  ▐▌ ▐▌                      ",
+  "//  ▐▌   ▝▚▄▞▘▐▌ ▐▌ █  ▐▌   ▝▚▄▞▘▐▙▄▄▖▗▄█▄▖▝▚▄▞▘                      ",
+  "//                                                                    ",
+  '<span class="color2">Last update: Jan 5, 2025</span>',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
